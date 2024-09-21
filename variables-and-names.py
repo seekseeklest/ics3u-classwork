0e3b2586c2758e8111dd5ee02c5f335ed2f8154f
seekseeklest <fluffycloud2008@gmail.com>
2024-09-21 14:39:44 -0400

    Update variables-and-names.py

--- variables-and-names.py
+++ variables-and-names.py
@@ -1,14 +1,25 @@
+# we are assigning the variable name "team" with a value of "Toronto Blue Jays" which is a string
 team = "Toronto Blue Jays"
+# the variable name "current_date" has been assigned a value of "July 18, 2021" which is a string
 current_date = "July 18, 2021"
+# the variable name "player" has been assigned a value of "Vladimir Guerrero Jr."
 player = "Vladimir Guerrero Jr."
+# the variable name "home_runs_to_date" is assigned an value of an integer, 31
 home_runs_to_date = 31
+# the variable named "games_played" is assigned a value of 88
 games_played = 88
+# the variable named "total_season_games" is assigned a value of 162
 total_season_games = 162
+# the variable named "home_run_record" is assigned a value of 73
 home_run_record = 73
 
+# the variable named "games_remaining" is assigned a value of total_season_games - games_played, which is equal to 162 - 88 = 74
 games_remaining = total_season_games - games_played
+# the variable named "home_runs_per_game" is assigned a value of home_runs_to_date / games_played, which is equal to 31 / 88 = 0.35227272727
 home_runs_per_game = home_runs_to_date / games_played
+# the variable named "projected_home_runs" is assigned a value of home_runs_per_game * total_season_games, which is equal to 0.35227272727 * 162 = 57.0681818182
 projected_home_runs = home_runs_per_game * total_season_games
+# the variable named "can_break_record" is assigned a value of when projected_home_runs is bigger than home_run_record
 can_break_record = projected_home_runs > home_run_record
 
 print(f"{player} of the {team}")
