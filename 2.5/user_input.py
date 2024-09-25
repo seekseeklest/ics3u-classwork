630003cf29c3c2149f60c0dc523cd605915c8342
seekseeklest <fluffycloud2008@gmail.com>
2024-09-24 21:39:01 -0400

    update user_input.py

--- 2.5/user_input.py
+++ 2.5/user_input.py
@@ -2,13 +2,12 @@
 print("Enter the following information about an item you wish to purchase..")
 print()
 
-print("The name of the item:")
 name = input()
+print("The name of the item:")
 
 price = float(input("The price: $"))
 
-print("How many do you want?")
-quantity = int(input())
+quantity = int(input("How many do you want?"))
 
 subtotal = price * quantity
 tax = subtotal * 0.13
@@ -16,4 +15,12 @@ total = subtotal + tax
 
 print()
 print(f"You choose to buy {quantity} {name}.")
-print(f"That will come out to ${total}")
+print("the price of your " + name + f" will come out to ${total}")
+
+# The first difference is that the name asks for the input without prompting for a data type, while the price input is specifically converted to a float. 
+# The second difference is that the name is printed as a separate statement, whereas the price input is combined with the prompt itself.
+
+# A prompt is a message displayed to the user that indicates what to enter for the input.
+# Switching the order can cause usability issues because the user will first see the input without the prompt, leading to confusion about what information is required. By the time they see the prompt, they may have already entered something incorrectly. Displaying the prompt first can lead to less mistakes.
+
+# inputs are normally saved as strings. Typing float and int will change the input to be saved as their respective data types. If they are not added, the code in the last group will not work, as multiplication between strings is impossible.
