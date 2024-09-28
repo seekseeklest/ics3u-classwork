d0cec6bbf0d57b15f2f32d79796dba20f8a41d8f
seekseeklest <fluffycloud2008@gmail.com>
2024-09-28 18:13:08 -0400

    update a_little_quiz.py

--- 3.2/a_little_quiz.py
+++ 3.2/a_little_quiz.py
@@ -1,36 +1,33 @@
+def check_answer(answer, correct_answer):
+    if answer == correct_answer:
+        print("Correct!")
+        return 1
+    else:
+        print("Incorrect!")
+        return 0
+
 def quiz():
-    print("Are you ready for a quiz? (Y/N)")
+    print("Are you ready for a Dark Souls quiz, Chosen Undead? (Y/N)")
     ready = input().lower()
     correct_answers = 0
+    
     if ready == 'n':
-        print("Well come back when you're ready!")
+        print("Well, come back when you're ready!")
         exit()
     elif ready == 'y':
-        print("Okay, here it comes!\n")
+        print("Get ready to kindle the flame!\n")
     else:
         print("Please type Y or N.")
         exit()
 
-    answer1 = int(input("\nQ1) What is the capital of Alaska?\n1) Melbourne\n2) Anchorage\n3) Juneau\n"))
-    if answer1 == 3:
-        print("Correct!")
-        correct_answers = correct_answers + 1
-    else:
-        print("Incorrect!")      
+    answer1 = int(input("\nQ1) Which of the following are not one of Gwyn's Four Knights?\n1) Artorias the Abysswalker\n2) Executioner Smough\n3) Lord's Blade Ciaran\n4) Hawkeye Gough\nAnswer: "))
+    correct_answers += check_answer(answer1, 2)
 
-    answer2 = int(input("\nQ2) In Python, the way you get keyboard input is the keyboard_input function.\n1) true\n2) false\n"))
-    if answer2 == 2:
-        print("Correct!")
-        correct_answers = correct_answers + 1
-    else:
-        print("Incorrect!")    
-    
-    answer3 = int(input("\nQ3) What is the result of 9 + 6 / 3?\n1) 5\n2) 11\n3) 15/3\n"))
-    if answer3 == 2:
-        print("Correct!")
-        correct_answers = correct_answers + 1
-    else:
-        print("Incorrect!")     
+    answer2 = int(input("\nQ2) In a failed attempt to recreate the First Flame, the Bed of Chaos was born. Which central figure was responsible for this?\n1) Gwyn, Lord of Cinder\n2) Aldia, Scholar of the First Sin\n3) Manus, Father of the Abyss\n4) Witch of Izalith\nAnswer: "))
+    correct_answers += check_answer(answer2, 4)
+
+    answer3 = int(input("\nQ3) Who is so easily forgotten?\n1) The Furtive Pygmy\n2) Covetous Demon\n3) Nameless King\n4) Seath the Scaleless\nAnswer: "))
+    correct_answers += check_answer(answer3, 1)
 
     print(f"\nYou got {correct_answers}/3 questions right!")
 
