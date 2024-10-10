960550d52af5431cffe450acebd01a4ded60135b
seekseeklest <fluffycloud2008@gmail.com>
2024-10-09 20:24:38 -0400

    update 4.1

---
+++ 4.1/while_loop_counter.py
@@ -0,0 +1,69 @@
+# Count from 0 to 10 by 1
+print("Counting from 0 to 10 by 1:")
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+
+# Count from 10 to 0 by 1
+print("\nCounting from 10 to 0 by 1:")
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+
+# Count from 491 to 586 by 5
+print("\nCounting from 491 to 586 by 5:")
+i = 491
+while i <= 586:
+    print(i)
+    i += 5
+
+# Count from 124 to 810 by 14
+print("\nCounting from 124 to 810 by 14:")
+i = 124
+while i <= 810:
+    print(i)
+    i += 14
+
+# Count from -233 to -113 by 4
+print("\nCounting from -233 to -113 by 4:")
+i = -233
+while i <= -113:
+    print(i)
+    i += 4
+
+# Count from 215 to 300 by 5
+print("\nCounting from 215 to 300 by 5:")
+i = 215
+while i <= 300:
+    print(i)
+    i += 5
+
+# Count from -310 to -94 by 8
+print("\nCounting from -310 to -94 by 8:")
+i = -310
+while i <= -94:
+    print(i)
+    i += 8
+
+# Count from -476 to -410 by 3
+print("\nCounting from -476 to -410 by 3:")
+i = -476
+while i <= -410:
+    print(i)
+    i += 3
+
+# Count from -327 to -147 by 9
+print("\nCounting from -327 to -147 by 9:")
+i = -327
+while i <= -147:
+    print(i)
+    i += 9
+
+# Count from 484 to 1024 by 18
+print("\nCounting from 484 to 1024 by 18:")
+i = 484
+while i <= 1024:
+    print(i)
+    i += 18
