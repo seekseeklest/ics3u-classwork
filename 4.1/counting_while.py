af9cc4e4c23a0441c125811b6fcfb7ea81a2af9b
seekseeklest <fluffycloud2008@gmail.com>
2024-10-09 20:26:59 -0400

    update counting_while.py

--- 4.1/counting_while.py
+++ 4.1/counting_while.py
@@ -1,11 +1,11 @@
-print("Type in a message, and I'll display it ten times.")
+print("Type in a message, and I'll display it ten times, counting up by ten each time.")
 
 message = input("Message: ")
 print()
 
 n = 0
 while n < 10:
-    print(f"{n + 1}. {message}")
+    print(f"{(n + 1) * 10}. {message}")
     n += 1
 
 # n += 1 will increase the value of variable n by 1. the code will continue looping until it has increased to no longer be smaller than 5.
