7730928cdff6d14cd7fcfecfd7c3955fba0d64c1
seekseeklest <fluffycloud2008@gmail.com>
2024-10-11 20:40:54 -0400

    create keep_guessing.py

---
+++ 4.3/keep_guessing.py
@@ -0,0 +1,23 @@
+import random
+n = random.randint(1,11)
+
+guesses = 0
+game_running = True
+
+print('I have chosen an integer between 1 and 10')
+while game_running:
+	input_num = int(input('Guess what it is: '))
+	if input_num:
+		op = int(input_num)
+		if op == n:
+			print('Congratulations, you guessed correctly!')
+			print(f'It took you {guesses+1} tries to get it right!')
+			game_running=False
+		elif op < n:
+			print('Oopsies! your number was too small! Please try again!')
+			guesses+=1
+		elif op > n:
+			print('Oopsies! your number was too big! Please try again!')
+			guesses+=1
+	else:
+		print('Sorry, I could not process that. Please type an integer between 1 and 10 and try again.')
